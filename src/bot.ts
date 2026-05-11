0f08b3311741bfec9763a7341751ae96420c4d10
KelpDev <134632438+KelpDevs@users.noreply.github.com>
2026-05-11 10:01:14 +0700

    update

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,5 @@
 import { Bot, webhookCallback } from "grammy";
-import { serveDir } from "https://jsr.io/@std/http/1.0.25/file_server.ts";
 
-const kv = await Deno.openKv();
 const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
 bot.command("start", (ctx) => ctx.reply("Selamat datang di Flyxteele Bot!"));
